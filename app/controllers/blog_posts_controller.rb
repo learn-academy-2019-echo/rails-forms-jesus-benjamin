e13ae779c96392fed009a4df846ed05a721f1bf2
learn <learn@learn11.local>
2019-11-12 16:02:59 -0800

    Add and Delete Blog Post

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -8,4 +8,28 @@ class BlogPostsController < ApplicationController
     @post = BlogPost.find(params[:id])
     #render "show.html.erb"
   end
+
+  def new
+    #rend "new.html.erb"
+  end
+
+  def create
+      @post = BlogPost.new(title: params[:title], content: params[:content])
+
+      if @post.save
+          redirect_to @post
+      else
+          render action: :new
+    end
+  end
+
+  def delete
+    @post = BlogPost.find(params[:id])
+    if @post.destroy
+        redirect_to '/blog_posts'
+    else
+        p "got to hell you bastard"
+    end
+  end
+
 end
