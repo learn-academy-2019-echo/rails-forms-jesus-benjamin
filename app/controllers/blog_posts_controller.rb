27b1e7ef3ece2d50e783a75bed3eb6c85d692b8c
learn <learn@learn11.local>
2019-11-13 13:28:09 -0800

    edit blog post complete

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -10,11 +10,21 @@ class BlogPostsController < ApplicationController
   end
 
   def new
+     @post = BlogPost.new
     #rend "new.html.erb"
   end
 
+  def edit
+     @post = BlogPost.find(params[:id])
+  end
+
+  def update
+      @post = BlogPost.find(params[:id])
+      @post.update(blog_post_params)
+      redirect_to @post
+  end
   def create
-      @post = BlogPost.new(title: params[:title], content: params[:content])
+      @post = BlogPost.new(blog_post_params)
 
       if @post.save
           redirect_to @post
@@ -31,5 +41,12 @@ class BlogPostsController < ApplicationController
         p "got to hell you bastard"
     end
   end
+
+  private
+     def blog_post_params
+       params
+         .require(:blog_post)
+         .permit([:title, :content])
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get "blog_posts" => "blog_posts#index", as: "all_posts"
   get "blog_posts/new" => "blog_posts#new"
   get "blog_posts/:id" => "blog_posts#show", as: "blog_post"
+  get "blog_posts/edit/:id" => "blog_posts#edit", as: "blog_edit"
   post "blog_posts" => "blog_posts#create"
   delete "blog_posts/:id" => "blog_posts#delete"
+  patch "blog_posts/:id" => "blog_posts#update"
   root to: 'blog_posts#index'
 end
